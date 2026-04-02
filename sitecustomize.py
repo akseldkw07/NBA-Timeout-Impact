a094af02276a0764654a36cf1781858397f437c9
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2026-04-02 16:48:04 +0000

    Tidy-Up Added: - .editorconfig Modified: - .gitignore Deleted: - macros.sty

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,1 +1,27 @@
-/Users/Akseldkw/coding/kretsinger/sitecustomize.py
+import warnings
+
+try:
+    # pydantic v2 - import the warnings module and resolve attributes at runtime
+    import pydantic.warnings as pydantic_warnings  # type: ignore
+
+    PydanticUserWarning = getattr(pydantic_warnings, "PydanticUserWarning", UserWarning)
+    UnsupportedFieldAttributeWarning = getattr(pydantic_warnings, "UnsupportedFieldAttributeWarning", Warning)
+
+    warnings.filterwarnings("ignore", category=UnsupportedFieldAttributeWarning)
+    # belt-and-suspenders in case subclasses vary
+    warnings.filterwarnings(
+        "ignore",
+        category=PydanticUserWarning,
+        message=r".*UnsupportedFieldAttributeWarning.*",
+    )
+except Exception:
+    pass
+
+# also quiet the MPS pin_memory message
+warnings.filterwarnings(
+    "ignore", message=r".*pin_memory.*", category=UserWarning, module=r"torch\.utils\.data\.dataloader"
+)
+
+
+# lgbm warnings
+warnings.filterwarnings("ignore", category=Warning, message=r".*No further splits with positive gai.*")
